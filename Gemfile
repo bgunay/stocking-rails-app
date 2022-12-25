86b768b76dcffdf92f671b553c83335575013800
bgunay <b.gunay1@gmail.com>
2022-12-26 02:45:48 +0300

    installed device and home, about pages created

--- Gemfile
+++ Gemfile
@@ -27,6 +27,10 @@ gem "stimulus-rails"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 
+# Bootstrap css
+gem 'bootstrap', '~> 5.2', '>= 5.2.3'
+gem 'jquery-rails'
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
@@ -70,3 +74,10 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+
+gem "cssbundling-rails", "~> 1.1"
+
+gem "jsbundling-rails", "~> 1.1"
+
+gem "stock_quote", '~> 3.0.0'
+gem 'devise', '~> 4.8', '>= 4.8.1'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,19 @@
 class HomeController < ApplicationController
   def index
+    StockQuote::Stock.new(api_key: "sk_914987cf1a434a9cbd1d081cf7f8ada0")
+    if params[:ticker] == ""
+      @nothing = "Hey, Your forgot to enter a ticker!"
+    elsif params[:ticker]
+      begin
+        @stock = StockQuote::Stock.quote(params[:ticker])
+      rescue => err
+        puts "stock error: #{err}"
+        @error = "This stock does not exist! Err ->  #{err}"
+      end
+    end
+
+    def about
+
+    end
   end
 end
